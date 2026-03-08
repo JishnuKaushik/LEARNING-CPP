53cc0b9b047bbd38283333caf64b17dfd52c7dcd
Jishnu Kaushik <jishnukaushik16@gmail.com>
2026-03-09 05:16:02 +0530

    Initial commit

---
+++ lec03(variables&output)/helloworld.cpp
@@ -0,0 +1,10 @@
+#include <iostream>  
+// # ke bad jo bhi kuch likha hai use directive bolte hai
+// iostream is one of the most commonly used cpp headerfile.
+using namespace std;
+
+int main()
+{// prints hello world in c+
+    cout << "Hello world in cpp";
+return 0;
+}
