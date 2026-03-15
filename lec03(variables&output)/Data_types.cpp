4e79eb2d1178f1d706399b4e6ddd473bcc851d1e
Jishnu Kaushik <jishnukaushik16@gmail.com>
2026-03-15 14:39:36 +0530

    15 March 2026

---
+++ lec03(variables&output)/Data_types.cpp
@@ -0,0 +1,35 @@
+/*
+1. integer data type 
+1byte = 8 bits
+bit = smallest unit of sorage.
+o,1
+1byte = 2^8
+4 byte = 2^8*4
+
+2. character data type
+1 byte
+
+3. boolean data type
+true or false
+bool flag = t or f
+1 byte
+
+4. floating point data type
+decimals, exponentials
+7 decimal places
+4 bytes
+
+5. double floating point data type 
+15 decimal places 
+8 byte
+
+6. void data ype 
+null or nothing 
+
+7 wide charater data type 
+2 byte
+
+8. string data type
+string name = "urvi";
+
+*/
---
+++ lec03(variables&output)/Identifires.cpp
@@ -0,0 +1,7 @@
+/*
+rule 1 all identifires should begin with a letter a to z ot A toZ , currency $ or an underscore
+rule 2 after the first character , identifires can have any combination of characters
+rule 3 a keyword cannot be used as an identifires.
+rule 4 the identifires are case sensitive
+rule 5 whitespace is not permitted
+*/
