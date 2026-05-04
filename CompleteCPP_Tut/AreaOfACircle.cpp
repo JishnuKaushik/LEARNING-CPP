1095643324e1de2387984805b0e8ca35044da490
Jishnu Kaushik <jishnukaushik16@gmail.com>
2026-05-04 05:36:04 +0530

    Add detailed comment explaining area calculation of a circle

--- CompleteCPP_Tut/AreaOfACircle.cpp
+++ CompleteCPP_Tut/AreaOfACircle.cpp
@@ -8,3 +8,4 @@ int main(){
     //cout<<"Area is :";
     //cout<<a<<endl;
 }
+//Area of a circle is given by the formula A = πr^2, where r is the radius of the circle. In this code, we are calculating the area of a circle with a radius of 4.3 using the value of π as 3.141592. The result is printed to the console.
